d8871d3eaf63479920e81fdcce02fc6b89412029
Sukoreno Mukti <sukorenomw@gmail.com>
2015-05-25 16:43:21 +0700

    fixed missing jquery asset

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require jquery.min
 //= require jquery.hotkey
 //= require_tree .
