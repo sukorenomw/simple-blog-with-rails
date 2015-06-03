5159da7f058f8dab5643019dd4fb6e094d4ea4cf
Sukoreno Mukti <sukorenomw@gmail.com>
2015-06-03 15:18:04 +0700

    moved sign up to popup modal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,28 @@ class UsersController < ApplicationController
 
   def new
   	@user = User.new
+    respond_to do | format |
+      format.html
+      format.js
+    end
   end
 
   def create
- 	@user = User.new(params_user)
- 	@user.valid?
- 	if simple_captcha_valid?
-	  	if @user.save
-	  		flash[:notice] = "User successfully added!"
-	  		redirect_to root_url
-	  	else
-	  		flash[:error] = "data not valid"
-	        render 'new'
-	  	end
-	else
+   	@user = User.new(params_user)
+   	@user.valid?
+    # respond_to do | format |
+     	if simple_captcha_valid?
+    	  	if @user.save
+      	  		flash[:notice] = "User successfully added!"
+      	  		render 'success'
+    	  	else
+              flash[:error] = "data not valid"
+              render 'new'
+    	  	end
+    	else
         flash[:errors] = "Please re-enter the recaptcha code below!" 
         render 'new'
-  	end
-  	# render :text => params_user
+    	end
   end
 
   def edit
